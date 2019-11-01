567b54a1f28bd724f45214d50f3d398cb50b066c
mosesprasanthjoy <mosesprasanthjoy@gmail.com>
2019-11-01 16:41:45 +0000

    commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import MinimizableWebChat from './MinimizableWebChat';
 
-import WebPageBackground from './WebPage.jpg';
+//import WebPageBackground from './WebPage.jpg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <img alt="product background" src={WebPageBackground} />
+         
         <MinimizableWebChat />
       </div>
     );
